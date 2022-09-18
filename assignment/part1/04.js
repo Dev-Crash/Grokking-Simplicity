99b53317d412fb59677b1390dc1da7266fb45795
eden.ju <public.jin03@gmail.com>
2022-09-18 22:52:41 +0900

    feat: part 1, 04번 문제 풀이

--- assignment/part1/04.js
+++ assignment/part1/04.js
@@ -1,11 +1,13 @@
 // `add_book`과 `delete_book` 함수를 카피-온-라이트 규칙을 사용해서 작성해봅시다.
 
-var bookList = ["노인과바다", "어린왕자", "파리대왕"];
+var bookList = ['노인과바다', '어린왕자', '파리대왕'];
 
 function add_book_item(bookList, book) {
-  bookList.push(book);
+  return [...bookList, book];
 }
 
 function delete_book_item(bookList, index) {
-  bookList.splice(index, 1);
+  return bookList.filter((_, idx) => idx !== index);
 }
+
+// 1. 카피 온 라이트의 경우에 전개 연산자나 배열의 고차 함수를 이용하면 쉽게 구현할 수 있습니다.
