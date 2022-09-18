373f0344d623af2a0666663321fa07a3dd3895db
eden.ju <public.jin03@gmail.com>
2022-09-18 22:45:06 +0900

    feat: part 1, 03번 문제 풀이

--- assignment/part1/03.js
+++ assignment/part1/03.js
@@ -2,25 +2,25 @@
 // * 참고사항 *
 // 재사용하기 쉽고, 유지보수하기 쉽고, 테스트하기 쉽도록
 
-var bookList = ["노인과바다", "어린왕자", "파리대왕"];
+var bookList = ['노인과바다', '어린왕자', '파리대왕'];
 
-function add_book(book) {
-  bookList.forEach((item) => {
-    if (item === book) {
-      return;
-    } else {
-      bookList.push(book);
-    }
-  });
-}
+const addUniqueItem = (array, item) => {
+  return [...new Set([...array, item])];
+};
 
-function delete_book(book) {
-  let deleteIdx = 0;
-  bookList.forEach((item, index) => {
-    if (item === book) {
-      deleteIdx = index;
-      return;
-    }
-  });
-  bookList.splice(deleteIdx, 1);
-}
+const add_book = (bookList, book) => {
+  return addUniqueItem(bookList, book);
+};
+
+const deleteItem = (array, item) => {
+  return array.filter((each) => each !== item);
+};
+
+const delete_book = (bookList, book) => {
+  return deleteItem(bookList, book);
+};
+
+// 1. add_book 함수의 경우 bookList 전역 변수에 접근하지 않고 인자를 받은 후에 복사하여 사용할 수 있도록 했습니다.
+// 2. 이 때, addUniqueItem 이라는 중복 검사 후에 item을 추가하는 유틸 함수를 만들고 이를 사용할 수 있도록 했습니다.
+// 3. 마찬가지로 delete_book 함수의 경우도 인자로 bookList를 받도록 했습니다.
+// 4. 이후에는 배열의 고차함수를 사용해서 카피 온 라이트 효과를 볼 수 있도록 했습니다.
